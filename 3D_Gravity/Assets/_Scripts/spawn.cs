0289dc75f7c2f573de15bcf6a74804767788db01
zhenga6 <zhenga6@bxscience.edu>
2018-12-01 17:01:49 -0500

    Level3
    complete

---
+++ 3D_Gravity/Assets/_Scripts/spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+    public GameObject obstacle;
+
+	// Use this for initialization
+	void Start () {
+        int i = 0;
+        while (i < 11) {
+            Instantiate(obstacle, transform);
+            i++;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
