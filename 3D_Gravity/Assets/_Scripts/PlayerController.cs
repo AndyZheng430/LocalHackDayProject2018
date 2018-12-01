2fdaf49b51f93fe55c7f283325d3f3b6c6593705
zhenga6 <zhenga6@bxscience.edu>
2018-12-01 13:10:19 -0500

    player controller
    collision involving shrink, expanding, and regular form

--- 3D_Gravity/Assets/_Scripts/PlayerController.cs
+++ 3D_Gravity/Assets/_Scripts/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        /*
+         Gravity change in a single direction for all objects including player
+         */
         if (Input.GetKey(KeyCode.UpArrow)) {
             Physics.gravity = new Vector3(0f, 3f);
         }
@@ -23,6 +26,18 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKey(KeyCode.RightArrow)) {
             Physics.gravity = new Vector3(3f, 0f);
         }
-
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("shrink")) {
+            gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        if (other.gameObject.CompareTag("expand")) {
+            gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        }
+        if (other.gameObject.CompareTag("regular")) {
+            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
 }
