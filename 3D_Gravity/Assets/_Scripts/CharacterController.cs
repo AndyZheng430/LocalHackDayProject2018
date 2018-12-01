b97fa6b769fc3eb2dd8116ba96efbfa909d2b4a9
zhenga6 <zhenga6@bxscience.edu>
2018-12-01 12:18:52 -0500

    Script name change

--- 3D_Gravity/Assets/_Scripts/CharacterController.cs
+++ 3D_Gravity/Assets/_Scripts/CharacterController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +11,18 @@ public class CharacterController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+
+        }
+
 	}
 }
