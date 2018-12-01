9d72745e646021fd6b79bafdfa5eb467ed50b3c2
zhenga6 <zhenga6@bxscience.edu>
2018-12-01 12:49:45 -0500

    Player Script
    add gravity controlling

--- 3D_Gravity/Assets/_Scripts/PlayerController.cs
+++ 3D_Gravity/Assets/_Scripts/PlayerController.cs
@@ -12,16 +12,16 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         if (Input.GetKey(KeyCode.UpArrow)) {
-            
+            Physics.gravity = new Vector3(0f, 3f);
         }
         if (Input.GetKey(KeyCode.DownArrow)) {
-
+            Physics.gravity = new Vector3(0f, -3f);
         }
         if (Input.GetKey(KeyCode.LeftArrow)) {
-
+            Physics.gravity = new Vector3(-3f, 0f);
         }
         if (Input.GetKey(KeyCode.RightArrow)) {
-
+            Physics.gravity = new Vector3(3f, 0f);
         }
 
 	}
